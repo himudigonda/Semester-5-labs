528ad9de8c965ef9ba163ade85c4aa10bd9a8f9d
ruhend <mudigonda.himanshh@gmail.com>
2021-08-19 18:59:44 +0530

    OS: answer 17

---
+++ CSE_301L-Operating Systems Lab/Assignment 3/answer17.sh
@@ -0,0 +1,22 @@
+# ! /bin/bash
+# @author : @ruhend (Mudigonda Himansh)
+# Assignment 3
+
+START=$HOME/Documents/
+
+# change your directory to command line if passed
+# otherwise use home directory
+[ $# -eq 1 ] && START=$1 || :
+
+if [ ! -d $START ]; then
+    echo "$START not a directory!"
+    exit 1
+fi
+
+# use find command to get all subdirs name in DIRS variable
+DIRS=$(find "$START" -type d)
+
+# loop thought each dir to get the number of files in each of subdir
+for d in $DIRS; do
+    [ "$d" != "." -a "$d" != ".." ] && echo "$d dirctory has $(ls -l $d | wc -l) files" || :
+done
