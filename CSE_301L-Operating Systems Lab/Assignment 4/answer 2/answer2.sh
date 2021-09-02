6a63c968273d245ef2cceb5c40d6425981ae73e3
ruhend <mudigonda.himanshh@gmail.com>
2021-09-02 17:45:18 +0530

    OS: Assignment 4 Answer 2 (removed redundant sed command)

--- CSE_301L-Operating Systems Lab/Assignment 4/answer 2/answer2.sh
+++ CSE_301L-Operating Systems Lab/Assignment 4/answer 2/answer2.sh
@@ -4,9 +4,9 @@
 
 FILE="testFile2.txt"
 
-count_a=$(grep -o " a " $FILE | wc -l | sed 's/[a-zA-z. ]//g')
-count_an=$(grep -o " an " $FILE | wc -l | sed 's/[a-zA-z. ]//g')
-count_the=$(grep -o " the " $FILE | wc -l | sed 's/[a-zA-z. ]//g')
+count_a=$(grep -o " a " $FILE | wc -l )
+count_an=$(grep -o " an " $FILE | wc -l )
+count_the=$(grep -o " the " $FILE | wc -l )
 
 # sum=$(count_a+count_an+count_the)
 
