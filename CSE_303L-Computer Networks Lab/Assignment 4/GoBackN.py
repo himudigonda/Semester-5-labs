e2a883e7f0e36b021274e7ba765146536a4396f0
ruhend <mudigonda.himanshh@gmail.com>
2021-09-12 21:38:19 +0530

    CN: Assignment 4

--- CSE_303L-Computer Networks Lab/Assignment 4/GoBackN.py
+++ CSE_303L-Computer Networks Lab/Assignment 4/GoBackN.py
@@ -4,10 +4,7 @@
 #! Go Back N
 
 """
- [summary]
-
-Returns:
-    [type]: [description]
+[Go Back N]
 """
 
 import sys
@@ -16,51 +13,84 @@ import string
 
 
 def InputFunc():
+    """
+    InputFunc [This Function is used to take declare input.]
+
+    Returns:
+        [int]: [windowSize]
+        [int]: [dataBits]
+        [int]: [Timeout]
+    """
     dataStr = '1010010011'
     dataStr = " ".join(dataStr)
     windowSize = 4
+    # Makes the input string to a list
     dataBits = [int(x) for x in dataStr.split()]
     Timeout = 5
+    # Returns the variables to the main function
     return windowSize, dataBits, Timeout
 
 
 def send(dataSent, index, windowSize):
+    """
+    send [summary]
+
+    Args:
+        dataSent ([list])
+        index ([int])
+        windowSize ([int])
+    """
     print("Data Sent  : ", dataSent)
     print("Data Index : ", index)
 
 
 def GBN(windowSize, DataBits, Timeout):
+    """
+    GBN [The main function that makes the program work]
+
+    Args:
+        windowSize ([int])
+        DataBits ([list])
+        Timeout ([int])
+    """
     lenDataBits = len(DataBits)
     i = 0
+    # To keep a track of the data being sent RN. This is the sliding window
     currentWindow = list()
     for i in range(windowSize):
-        currentWindow.append(DataBits[i])
-        # ^ print("appended", DataBits[i])
+        currentWindow.append(DataBits[i])  # Filling the window with elements
     i += 1
-    while i <= len(DataBits)+windowSize:
-        # ^ print(i)
-        if len(currentWindow) <= windowSize:
-            if currentWindow != []:
+    while i <= lenDataBits+windowSize:  # While loop to loop over the elements
+        if len(currentWindow) <= windowSize:  # To Handle Edge Cases
+            if currentWindow != []:  # If the currentWindow size is not empty...
                 print(currentWindow)
+                # Do the parallel user(Receiver) input thingy
                 a, o, e = select.select([sys.stdin], [], [], Timeout)
-                if (a):
-                    ack = sys.stdin.readline().strip()
+                if (a):  # If input is provided
+                    ack = sys.stdin.readline().strip()  # Read the input
+                    # if the ack matches the index of the databit sent, then accept
                     if ack == str(i-windowSize):
+                        # This could be anything, I've taken this as it would be easier to evaluate
                         print(" + Ack Received!")
-                        currentWindow.pop(0)
-                        if i < len(DataBits):
+                        currentWindow.pop(0)  # Popping the sent element
+                        if i < lenDataBits:  # If next elements exist, append them
                             currentWindow.append(DataBits[i])
-                        i += 1
+                        i += 1  # Incremented i
                     else:
                         print(" - Ack Error... Sending window elements again...")
+                        #  Nothing needs to be done as i is not incremented
                 else:
                     print(" - Timeout Error")
+                    #  Nothing needs to be done as i is not incremented
             else:
                 print("**** End of Transmission ****")
                 break
 
 
 def main():
+    """
+    main [Main Function]
+    """
     print("**** Start of Transmission ****")
     windowSize, dataBits, Timeout = InputFunc()
     GBN(windowSize, dataBits, Timeout)
--- CSE_303L-Computer Networks Lab/Assignment 4/SelectiveRepeat.py
+++ CSE_303L-Computer Networks Lab/Assignment 4/SelectiveRepeat.py
@@ -16,6 +16,14 @@ import string
 
 
 def InputFunc():
+    """
+    InputFunc [This Function is used to take declare input.]
+
+    Returns:
+        [int]: [windowSize]
+        [int]: [dataBits]
+        [int]: [Timeout]
+    """
     dataStr = '1010010011'
     dataStr = " ".join(dataStr)
     windowSize = 4
@@ -24,62 +32,77 @@ def InputFunc():
     return windowSize, dataBits, Timeout
 
 
-def send(dataSent, index, windowSize):
-    print("Data Sent  : ", dataSent)
-    print("Data Index : ", index)
+def SR(windowSize, DataBits, Timeout):
+    """
+    SRN [The main function that makes the program work]
 
-
-def GBN(windowSize, DataBits, Timeout):
+    Args:
+        windowSize ([int])
+        DataBits ([list])
+        Timeout ([int])
+    """
     lenDataBits = len(DataBits)
     i = 0
-    ackIssueFlag = False
+    ackIssueFlag = False  # A Status Flag to mark when an issue occured
+    # To keep a track of the data being sent RN. This is the sliding window
     currentWindow = list()
     for i in range(windowSize):
-        currentWindow.append(DataBits[i])
-        # ^ print("appended", DataBits[i])
+        currentWindow.append(DataBits[i])  # Filling the window with elements
     i += 1
-    while i <= len(DataBits)+windowSize:
-        # ^ print(i)
-        if len(currentWindow) <= windowSize:
-            if currentWindow != []:
-                if ackIssueFlag == False:
-                    if i <= len(DataBits):
+    while i <= lenDataBits+windowSize:  # While loop to loop over the elements
+        if len(currentWindow) <= windowSize:  # To Handle Edge Cases
+            if currentWindow != []:  # If the currentWindow size is not empty...
+                if ackIssueFlag == False:  # If the status Flag is not triggered...
+                    if i <= lenDataBits:   # While loop to loop over the elements below the window size limit
+                        # ^ These below apf to cover Edge Cases
                         print(" > Sending Element with Indeces : ",
                               i-windowSize, "to", i-1)
-                    elif i == len(DataBits):
+                    elif i == lenDataBits:
                         print(" > Sending Element with Indeces : ", i-windowSize)
                     else:
                         print(" > Sending Element with Indeces : ",
-                              i-windowSize, "to", len(DataBits)-1)
+                              i-windowSize, "to", lenDataBits-1)
                     print(currentWindow)
-                if ackIssueFlag:
-                    print(" > Re-Sending Element with Index : ", i-windowSize)
+                if ackIssueFlag:  # If the status Flag is triggered...
+                    print(" > Re-Sending Element with Index : ",
+                          i-windowSize)  # Init re-send
                     print(" ± Resending", i-windowSize,
                           "th bit with issue : ", DataBits[i-windowSize])
+                # Do the parallel user(Receiver) input thingy
                 a, o, e = select.select([sys.stdin], [], [], Timeout)
-                if (a):
+                if (a):   # If input is provided
                     ack = sys.stdin.readline().strip()
+                    # if the ack matches the index of the databit sent, then accept
                     if ack == str(i-windowSize):
+                        # This could be anything, I've taken this as it would be easier to evaluate
                         print(" + Ack Received!")
-                        currentWindow.pop(0)
-                        if i < len(DataBits):
+                        currentWindow.pop(0)  # Popping the sent element
+                        if i < lenDataBits:  # If next elements exist, append them
                             currentWindow.append(DataBits[i])
-                        i += 1
-                        ackIssueFlag = False
+                        i += 1  # Incremented i
+                        ackIssueFlag = False  # Reset Trigger
                     else:
                         print(" - Ack Error... Sending window elements again...")
-                        ackIssueFlag = True
+                        ackIssueFlag = True  # Activate Trigger as Issue has occured
                 else:
+                    # No need to Activate Trigger here
                     print(" - Timeout Error")
+                    # ? Why is it not necessary?
+                    # Since we are implementing a Sliding Window protocol, for timeout the entire window is retransmitted
+                    # There are cases, where only one bit might get lost, but would require Parallel Programming, and Multi-Threading.
+                    #  Since, the objective is to code the logic of the protocol, This Edge Case is ignored.
             else:
                 print("**** End of Transmission ****")
                 break
 
 
 def main():
+    """
+    main [Main Function]
+    """
     print("**** Start of Transmission ****")
     windowSize, dataBits, Timeout = InputFunc()
-    GBN(windowSize, dataBits, Timeout)
+    SR(windowSize, dataBits, Timeout)
 
 
 if __name__ == '__main__':
